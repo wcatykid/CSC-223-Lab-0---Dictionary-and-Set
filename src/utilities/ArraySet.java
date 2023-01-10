7dd2b5290498a8c8b3c0b63d3bfbbd5908790fa2
wcatykid <ctalvin@gmail.com>
2023-01-10 16:15:31 -0500

    Ignore Updated

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -1,11 +1,156 @@
 package utilities;
 
-public class ArraySet
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.Spliterator;
+
+public class ArraySet<E> implements List<E>, Set<E>
 {
+	protected ArrayList<E> _list;
 
 	public ArraySet()
 	{
-		// TODO Auto-generated constructor stub
+		_list = new ArrayList<E>();
 	}
 
+	public ArraySet(Collection<E> collection)
+	{
+		this();
+
+		for (E item : collection)
+		{
+			add(item);
+		}
+	}
+
+	@Override
+	public int size() { return _list.size(); }
+
+	@Override
+	public boolean isEmpty() { return _list.isEmpty(); }
+
+	@Override
+	public boolean contains(Object o) { return _list.contains(o); }
+
+	@Override
+	public Iterator<E> iterator() { return _list.iterator(); }
+
+	@Override
+	public Object[] toArray() { return _list.toArray(); }
+
+	@Override
+	public <T> T[] toArray(T[] a) { return _list.toArray(a); }
+
+	@Override
+	public boolean add(E e)
+	{
+		if (_list.contains(e)) return false;
+
+		_list.add(e);
+		return true;
+	}
+
+	@Override
+	public boolean remove(Object o) { return _list.remove(o); }
+
+	@Override
+	public boolean containsAll(Collection<?> c) { return _list.containsAll(c); }
+
+	@Override
+	public boolean addAll(Collection<? extends E> collection)
+	{
+		boolean changed = false;
+		for (E item : collection)
+		{
+			if (add(item)) changed = true;
+		}
+		return changed;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> collection)
+	{
+		boolean changed = false;
+		for (int index = 0; index < _list.size(); )
+		{
+			if (collection.contains(_list.get(index)))
+			{
+				changed = true;
+				_list.remove(index);
+			}
+			else index++;
+		}
+		return changed;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> collection)
+	{
+		boolean changed = false;
+		for (Object item : collection)
+		{
+			if (remove(item)) changed = true;
+		}
+		return changed;
+	}
+
+	@Override
+	public void clear() { _list.clear(); }
+	
+	@Override
+	public boolean addAll(int index, Collection<? extends E> collection)
+	{
+		boolean changed = false;
+
+		int currIndex = index;
+		for (E item : collection)
+		{
+			if (!_list.contains(item))
+			{
+				changed = true;
+				_list.add(currIndex, item);
+				currIndex++;
+			}
+		}
+
+		return changed;
+	}
+
+	@Override
+	public E get(int index) { return _list.get(index); }
+
+	@Override
+	public E set(int index, E element) { return _list.set(index, element); }
+
+	@Override
+	public void add(int index, E element) { _list.add(index, element); }
+
+	@Override
+	public E remove(int index) { return _list.remove(index); }
+
+	@Override
+	public int indexOf(Object o) { return _list.indexOf(o); }
+
+	@Override
+	public int lastIndexOf(Object o) { return _list.lastIndexOf(o); }
+
+	@Override
+	public ListIterator<E> listIterator() { return _list.listIterator(); }
+
+	@Override
+	public ListIterator<E> listIterator(int index) { return _list.listIterator(index); }
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) { return _list.subList(fromIndex, toIndex); }
+
+	@Override
+	public Spliterator<E> spliterator()
+	{
+		// TODO Auto-generated method stub
+		return Set.super.spliterator();
+	}
 }
--- src/utilities/ParallelArrayDictionary.java
+++ src/utilities/ParallelArrayDictionary.java
@@ -1,11 +1,109 @@
 package utilities;
 
-public class ParallelArrayDictionary
-{
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
+public class ParallelArrayDictionary<Key, Value> implements Map<Key, Value>
+{
+    protected ArraySet<Key>    _keys;
+    protected ArrayList<Value> _values;
+	
 	public ParallelArrayDictionary()
 	{
-		// TODO Auto-generated constructor stub
+		_keys = new ArraySet<Key>();
+		_values = new ArrayList<Value>();
 	}
 
+	@Override
+	public int size() { return _keys.size(); }
+
+	@Override
+	public boolean isEmpty() { return _keys.isEmpty(); }
+
+	@Override
+	public boolean containsKey(Object key) { return _keys.contains(key); }
+
+	@Override
+	public boolean containsValue(Object value) { return _values.contains(value); }
+
+	@Override
+	public Value get(Object key)
+	{
+		int index = _keys.indexOf(key);
+		
+		return index == -1 ? null : _values.get(index);
+	}
+
+	@Override
+	public Value put(Key key, Value value)
+	{
+		int index = _keys.indexOf(key);
+	
+		// Overwrite previous Value and return that previous value
+		if (index != -1)
+		{
+			Value oldValue = _values.get(index);
+			
+			_values.set(index, value);
+			
+			return oldValue;
+		}
+		
+		_keys.add(key);
+		_values.add(value);
+		return null;
+	}
+
+	@Override
+	public Value remove(Object key)
+	{
+		int index = _keys.indexOf(key);
+
+		if (index == -1) return null;
+		
+		_keys.remove(index);
+		
+		Value oldValue = _values.get(index);
+		_values.remove(index);
+		
+		return oldValue;
+	}
+
+	@Override
+	public void putAll(Map<? extends Key, ? extends Value> m)
+	{
+		for (Map.Entry<? extends Key, ? extends Value> pair : m.entrySet())
+		{
+			put(pair.getKey(), pair.getValue());
+		}
+	}
+
+	@Override
+	public void clear()
+	{
+		_keys.clear();
+		_values.clear();
+	}
+
+	@Override
+	public Set<Key> keySet() { return _keys; }
+
+	@Override
+	public Collection<Value> values() { return _values; }
+
+	@Override
+	public Set<Entry<Key, Value>> entrySet()
+	{
+		Set<Entry<Key, Value>> set = new ArraySet<Entry<Key, Value>>();
+		
+		for (int i = 0; i < _keys.size(); i++)
+		{
+			set.add(new AbstractMap.SimpleEntry<Key, Value>(_keys.get(i), _values.get(i)));
+		}
+		
+		return set;
+	}
 }
